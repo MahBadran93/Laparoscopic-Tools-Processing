5cc19bf70ac460dc8dd19fc2727175dfc04854c3
MahBadran93 <mahmoud.s.badran@gmail.com>
2021-02-21 17:11:26 +0100

    edit data

--- network.py
+++ network.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
-
+import torch
 #import torch 
 #import torch.nn as nn
 #import torch.nn.functional as F 
@@ -13,17 +13,13 @@ from tensorflow import keras
 from tensorflow.keras.layers import Input, Dense
 from dataload import DataLoader
 
+
+#print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 # run using gpu or cpu 
+
 with tf.device('/cpu:0'):
 
-    inputEndo = Input(shape=(480, 640, 3))
-
-
-
-    vgg_Base = keras.applications.vgg16.VGG16(weights = 'imagenet',
-                        include_top = False,
-                        input_tensor = inputEndo) 
-        
+    #.............................Data Set...........................................
     data = DataLoader()
     masks_class,masks_inst, raw = data.loadDataRigid('./Data') 
 
@@ -31,18 +27,78 @@ with tf.device('/cpu:0'):
     masks_class = keras.applications.vgg16.preprocess_input(masks_class)
     raw = keras.applications.vgg16.preprocess_input(raw)
 
+
+    # .....................Input shape that will enter the network.....................
+    inputEndo = Input(shape=(480, 640, 3))
+
+
+    # ...................Feature Extraction Generator Block............................    
+    nClasses = 2                          
+    FrG = SeparableConv2D(filters = 64,
+                          kernel_size = (3, 3), 
+                          activation = 'relu',
+                          kernel_initializer='glorot_uniform',
+                          padding="same")(img_input)
+    FrG = BatchNormalization()(FrG)
+    
+    FrG = SeparableConv2D(filters = 256,
+                          kernel_size = (3, 3),
+                          activation = 'relu', 
+                          kernel_initializer='glorot_uniform', 
+                          padding="same")(FrG)
+    FrG = BatchNormalization()(FrG)
+        
+    FrG = SeparableConv2D(filters = 64,
+                          kernel_size = (3, 3), 
+                          activation = 'relu',
+                          kernel_initializer='glorot_uniform',
+                          padding="same")(FrG)
+    FrG = BatchNormalization()(FrG)
+    
+    FrG = SeparableConv2D(filters = nClasses,
+                          kernel_size = (3, 3), 
+                          activation = 'relu',
+                          kernel_initializer='glorot_uniform',
+                          padding="same")(FrG)
+    FrG = BatchNormalization()(FrG)
+
+
+    #...............................................................................
+
+    #........................ Encoeder vgg16 with dense output layer................
+    #.......................... Detection part......................................
+
+    # Pretrined model, trained on 'imagenet' dataset. THE ENCODER    
+    vgg_Base = keras.applications.vgg16.VGG16(weights = 'imagenet',
+                        include_top = False,
+                        input_tensor = inputEndo)
+
+    
+
+    # convert functional model to sequential model. 
+    vgg_sequential = keras.Sequential()
+    for layer in vgg_Base.layers:
+        vgg_sequential.add(layer)
+
+    # Freeze the layers in case of any future training because of fine tuning 
+    for layer in vgg_sequential.layers:
+        layer.trainable = False
+
+    # add flatten lyer to vgg16 to feed its output to Dense layer 
+    vgg_sequential.add(keras.layers.Flatten())
+    vgg_sequential.add(keras.layers.Dense(2,activation='softmax'))    
+
+    #........................................................................................
+     
+   
+
+    #vgg_Base.predict(raw)
     #plt.imshow(raw[0])
     #plt.show()
 
-    vgg_Base.compile(optimizer=keras.optimizers.Adam(lr=0.001, decay=1e-6) ,
-                loss=keras.losses.categorical_crossentropy, 
-                metrics = 'accuracy')
-    #print(vgg_Base.summary())      
-            
-    model = keras.Sequential(vgg_Base)
-    model.add(Dense(2,activation="relu"))
-    model.pop()
-    print(model.summary())
+    
+    print(vgg_sequential.summary())      
+
 
     with tf.device('/gpu:0'):
         pass
@@ -55,52 +111,3 @@ with tf.device('/cpu:0'):
 #vgg_Base.fit(x=raw, y=masks_inst, batch_size=20,epochs=100)
 
 
-
-'''
-def parse_cfg(cfgfile):
-    """
-    Takes a configuration file
-    
-    Returns a list of blocks. Each blocks describes a block in the neural
-    network to be built. Block is represented as a dictionary in the list
-    
-    """ 
-    
-    file = open(cfgfile, 'r')
-    lines = file.read().split('\n')                        # store the lines in a list
-    lines = [x for x in lines if len(x) > 0]               # get read of the empty lines 
-    lines = [x for x in lines if x[0] != '#']              # get rid of comments
-    lines = [x.rstrip().lstrip() for x in lines]           # get rid of fringe whitespaces
-    
-    
-    block = {}
-    blocks = []
-    
-    for line in lines:
-        if line[0] == "[":               # This marks the start of a new block
-            if len(block) != 0:          # If block is not empty, implies it is storing values of previous block.
-                blocks.append(block)     # add it the blocks list
-                block = {}               # re-init the block
-            block["type"] = line[1:-1].rstrip()     
-        else:
-            key,value = line.split("=") 
-            block[key.rstrip()] = value.lstrip()
-    blocks.append(block)
-    
-    return blocks
-
-
-# blocks  = parse_cfg('./yolov3.cfg')
-
-# for index, x in enumerate(blocks[1:]):
-#     module = nn.Sequential()
-    
-#     print(type(module))
-
-def create_modules(blocks):
-    net_info = blocks[0]     #Captures the information about the input and pre-processing    
-    module_list = nn.ModuleList()
-    prev_filters = 3
-    output_filters = []
-    
-'''
